012a50ab38bb2c61eed24e66c865a5e48ece9442
B326_Instructor <B326_Instructor@lab.lagcc>
2026-09-14 12:06:45 -0400

    Initial commit

---
+++ src/com/mac286/arrays/IntroArrays.java
@@ -0,0 +1,49 @@
+package com.mac286.arrays;
+
+public class IntroArrays {
+    static void main() {
+        //An array is a collection of variables that can be accessed using an index.
+        //To define an array we use the following: type[] arrayName = ...
+        int[] Ar = {-2, -5, -7, -8}; //this is a constant array
+        System.out.println("The length is: " + Ar.length);
+        System.out.println("The value of the third element is: " + Ar[2]);
+        //display all elements of the array in the following form: [-2, -5, -7, -8]
+        //diaplay a bracket without going to next line
+        System.out.print("[");
+        //loop element comma, ...
+        for(int i = 0; i < 4; i++){
+            if(i == 3){
+                System.out.print(Ar[i]);
+            }else {
+                System.out.print(Ar[i] + ", ");
+            }
+        }
+        //display a closing bracket, go to next line
+        System.out.println("]");
+
+        //We have a problem with the last comma. How to solve that??
+        //1. solution: use if: if last element don't display comma
+        //Not very efficient, there one extra test everytime for one comma!!!
+        //2. Deal with n-1 elements then deal with the last exception seprately
+        System.out.print("[");
+        //loop element comma, ...
+        for(int i = 0; i < 3; i++){
+            System.out.print(Ar[i] + ", ");
+        }
+        //display the last one without comma
+        System.out.print(Ar[3]);
+        //display a closing bracket, go to next line
+        System.out.println("]");
+
+        //We can also use the pattern comma number except for the first which is going
+        //to be delt with separately.
+        System.out.print("[" + Ar[0]);
+        //loop comma element, ....
+        for(int i = 1; i < 4; i++){
+            System.out.print(", " +Ar[i]);
+        }
+        //display a closing bracket, go to next line
+        System.out.println("]");
+
+    }
+}
---
+++ src/com/mac286/arrays/OurArray.java
@@ -0,0 +1,18 @@
+package com.mac286.arrays;
+/*
+Design a class OurArray that has the following properties:
+- A member array of integers
+- A variable size that keeps track of the number of elements added to the array
+- A default constructor that creates an array of 10 elements and sets size to 0
+- A getter fo size (int size() returns the size of the array)
+- A method boolean isEmpty() returns true if the array is empty and false if not.
+- A method void add(int e) adds element e to the end of the array (index size)
+- A method toString() that returns the content of the array in the form [-2, -5, -7, -8]
+- A method int remove() that removes the last element (index size-1) and returns it.
+- A method int get(int i) returns the element at index i, if i is valid index, otherwise
+it throws an exception.
+
+- Write a main where you test your class.
+ */
+public class OurArray {
+}
