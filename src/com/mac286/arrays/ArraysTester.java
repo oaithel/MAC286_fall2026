b9f6a10e1ea084f56363bbd5f0f752d667a87050
momo <oaithel@yahoo.com>
2026-09-16 10:51:25 -0400

    9/16/2026

---
+++ src/com/mac286/arrays/ArraysTester.java
@@ -0,0 +1,8 @@
+package com.mac286.arrays;
+
+public class ArraysTester {
+    public static void main(String[] args) {
+        //create an object OurArray, add to it -2, -5, -7 and -9
+        //Display it
+    }
+}
--- src/com/mac286/arrays/OurArray.java
+++ src/com/mac286/arrays/OurArray.java
@@ -14,14 +14,46 @@ it throws an exception.
 
 - Write a main where you test your class.
  */
+import java.util.Arrays;
 public class OurArray {
     //declare a variable for an array of integers
-
+    private int[] array;
     //declare a variable for size
-
-    //defualt constructor, create an array of 10 integers. Set size to 0
-
+    private int size;
+    //default constructor, create an array of 10 integers. Set size to 0
+    public OurArray() {
+        array = new int[10];
+        size = 0;
+    }
     //constructor that accepts an initial capacity for the array. Create an array
     //of that capacity and set size to 0
+    public OurArray(int c) {
+        array = new int[c];
+        size = 0;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return (size == 0);
+    }
+    public void add(int e){
+        if(size == array.length){
+           array = Arrays.copyOf(array, array.length*2);
+        }
+        array[size] = e;
+        size++;
+    }
+    public String toString(){
+        if(this.isEmpty()){
+            return "[]";
+        }
+        String str = "[" + array[0];
+        for(int i = 1; i < size; i++){
+            str += ", " + array[i];
+        }
+        str += "]";
+        return str;
+    }
 
 }
